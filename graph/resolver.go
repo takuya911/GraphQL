9e34b530f68d9f8cefd2a73c75d56abd5815a763
takuya911 <takuya.engineer911@gmail.com>
2020-09-13 09:14:48 +0900

    動くように変更

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,8 @@
 package graph
 
-// This file will not be regenerated automatically.
-//
-// It serves as dependency injection for your app, add any dependencies you require here.
+import "github.com/takuya911/GraphQL/graph/model"
 
-type Resolver struct{}
+// Resolver struct
+type Resolver struct {
+	todos []*model.Todo
+}
--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,17 +6,24 @@ package graph
 import (
 	"context"
 	"fmt"
+	"math/rand"
 
 	"github.com/takuya911/GraphQL/graph/generated"
 	"github.com/takuya911/GraphQL/graph/model"
 )
 
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	todo := &model.Todo{
+		Text: input.Text,
+		ID:   fmt.Sprintf("T%d", rand.Intn(1000)),
+		User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.todos, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
